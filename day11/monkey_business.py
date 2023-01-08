e1bf3423a924d5c5d7b1162102d50757b69c31cd
afraz98 <afraz98@umd.edu>
2023-01-07 20:15:32 -0500

    Day 11 part 2 solution

--- day11/monkey_business.py
+++ day11/monkey_business.py
@@ -58,8 +58,12 @@ class Monkey():
         old = self.items[index]
         self.items[index] = eval(self.operation)
     
-    def decrease_worry(self, index):
-        self.items[index] = self.items[index] // 3
+    def decrease_worry(self, index, factor):
+        # Part 1
+        # self.items[index] = self.items[index] // 3
+
+        # Part 2
+        self.items[index] = self.items[index] % factor
 
     def test_item(self, index):
         return self.items[index] % self.test == 0
@@ -78,10 +82,13 @@ class Monkey():
     
 monkeys = []
 
+# Divide item worry level by common denominator of 'inspection' numbers for part 2
+common_denominator = 1
+
 # Parse input for monkeys
 input = [line.strip("\n\t ") for line in open("monkey_business.txt", "r")]
 
-for line in input:
+for line in input:    
     if line[:6] == "Monkey":
         monkeys.append(Monkey(id=len(monkeys)))
     if line[:14] == "Starting items":
@@ -89,11 +96,15 @@ for line in input:
     if line[:9] == "Operation":
         monkeys[-1].operation = line[17:]
     if "Test" in line:
-        monkeys[-1].test = int(''.join(c for c in line if c.isdigit()))
+        denom = int(''.join(c for c in line if c.isdigit()))
+        monkeys[-1].test = denom
+        common_denominator = common_denominator * denom
     if "true" in line:
         monkeys[-1].is_true = int(''.join(c for c in line if c.isdigit()))
     if "false" in line:
         monkeys[-1].is_false = int(''.join(c for c in line if c.isdigit()))
+
+print(common_denominator)
 
 def perform_round():
     for monkey in monkeys:
@@ -108,8 +119,8 @@ def perform_round():
             print("\t\tWorry level increases to %d." % monkey.items[index])
 
             # Decrease worry level
-            monkey.decrease_worry(index)
-            print("\t\tMonkey gets bored with the item. Worry level divided by 3 to %d." % monkey.items[index])
+            monkey.decrease_worry(index, common_denominator)
+            print("\t\tMonkey gets bored with the item. Worry level decreased to %d." % monkey.items[index])
             
             # Find throw target
             print("\t\tCurrent worry level is %s divisible by %d." % ("" if monkey.test_item(index) else "not", monkey.test))
@@ -127,9 +138,9 @@ def perform_round():
     pass
 
 
-rounds = 20
+rounds = 10000
 for _ in range(rounds):
     perform_round()
 
 for monkey in monkeys:
-    print(monkey.items_inspected)
+    print("Monkey %d inspected %d items" % (monkey.id, monkey.items_inspected))
