8aaab819b8fbe642b50c2d6bbc25f7f0694b04e7
afraz98 <afraz98@umd.edu>
2023-01-15 17:48:21 -0500

    Day 12 part 2 solution

--- day12/hill_climb.py
+++ day12/hill_climb.py
@@ -99,15 +99,11 @@ def trace_path(cells, end_x, end_y):
     while not (cells[row][col].parent_x == row and cells[row][col].parent_y == col):
         path.insert(0, cells[row][col])
 
-        print(row, col, grid[row][col])
-
         new_row = cells[row][col].parent_x 
         new_col = cells[row][col].parent_y
 
         row = new_row
         col = new_col
-        
-        print(row, col, grid[row][col])
 
     path.insert(0, cells[row][col])
     while path != []:
@@ -126,6 +122,9 @@ def climb_hill(start_x, start_y, end_x, end_y):
         start_y (int): Starting y coordinate
         end_x (int): End x coordinate
         end_y (int): End y coordinate
+
+    Returns:
+        (int): Path length
     """
     grid[start_x][start_y] = 'a'
     grid[end_x][end_y] = 'z'
@@ -148,7 +147,6 @@ def climb_hill(start_x, start_y, end_x, end_y):
 
     while open != []:
         cell = open.pop(0)
-        print(cell.x, cell.y, grid[cell.x][cell.y])
         closed[cell.x][cell.y] = True
 
         # 'North' successor
@@ -157,7 +155,7 @@ def climb_hill(start_x, start_y, end_x, end_y):
 
                 # 'North' successor is destination cell?
                 if cell.x - 1 == end_x and cell.y == end_y and ord(grid[cell.x - 1][cell.y]) - ord(grid[cell.x][cell.y]) <= 1:
-                    print("Found destination")
+                    # print("Found destination")
                     cells[cell.x - 1][cell.y].parent_x = cell.x
                     cells[cell.x - 1][cell.y].parent_y = cell.y
                     return trace_path(cells, end_x, end_y)
@@ -182,7 +180,7 @@ def climb_hill(start_x, start_y, end_x, end_y):
                 
                 # 'South successor is desination cell?
                 if cell.x + 1 == end_x and cell.y == end_y and ord(grid[cell.x + 1][cell.y]) - ord(grid[cell.x][cell.y]) <= 1:
-                    print("Found destination") 
+                    # print("Found destination") 
                     cells[cell.x + 1][cell.y].parent_x = cell.x
                     cells[cell.x + 1][cell.y].parent_y = cell.y
                     return trace_path(cells, end_x, end_y)
@@ -207,7 +205,7 @@ def climb_hill(start_x, start_y, end_x, end_y):
                 
                 # 'South successor is desination cell?
                 if cell.x == end_x and cell.y + 1 == end_y and ord(grid[cell.x][cell.y + 1]) - ord(grid[cell.x][cell.y]) <= 1:
-                    print("Found destination")
+                    # print("Found destination")
                     cells[cell.x][cell.y + 1].parent_x = cell.x
                     cells[cell.x][cell.y + 1].parent_y = cell.y
                     return trace_path(cells, end_x, end_y)
@@ -232,7 +230,7 @@ def climb_hill(start_x, start_y, end_x, end_y):
                 
                 # 'South successor is desination cell?
                 if cell.x == end_x and cell.y - 1 == end_y and ord(grid[cell.x][cell.y - 1]) - ord(grid[cell.x][cell.y]) <= 1:
-                    print("Found destination") 
+                    # print("Found destination") 
                     cells[cell.x][cell.y - 1].parent_x = cell.x
                     cells[cell.x][cell.y - 1].parent_y = cell.y
                     return trace_path(cells, end_x, end_y)
@@ -250,6 +248,40 @@ def climb_hill(start_x, start_y, end_x, end_y):
                         cells[cell.x][cell.y - 1].parent_x = cell.x
                         cells[cell.x][cell.y - 1].parent_y = cell.y
                         open.append(cells[cell.x][cell.y - 1])
-    pass
+    return 999
 
-print(climb_hill(start_x, start_y, end_x, end_y))
+"""
+--- Part Two ---
+
+As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
+
+To maximize exercise while hiking, the trail should start as low as possible: elevation a. The goal is still the square marked E. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from any square at elevation a to the square marked E.
+
+Again consider the example from above:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+
+Now, there are six choices for starting position (five marked a, plus the square marked S that counts as being at elevation a). If you start at the bottom-left square, you can reach the goal most quickly:
+
+...v<<<<
+...vv<<^
+...v>E^^
+.>v>>>^^
+>^>>>>>^
+
+This path reaches the goal in only 29 steps, the fewest possible.
+
+What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
+"""
+starting_positions = []
+
+for i in range(height):
+    for j in range(width):
+        if grid[i][j] == 'a':
+            starting_positions.append((i,j))
+
+print(min([climb_hill(start[0], start[1], end_x, end_y) for start in starting_positions]))
